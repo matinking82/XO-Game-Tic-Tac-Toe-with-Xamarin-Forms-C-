4d62e92b5c7b39cc75d237cb43cefe25168ea1f5
Matin Babaki <matinking82@gmail.com>
2021-02-28 22:40:04 +0330

    Add ShowToast Class

---
+++ XO/XO.Android/Utilities/ShowToast.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XO.Utilities;
+
+namespace XO.Droid.Utilities
+{
+    public class ShowToast : IShowToast
+    {
+        public void ShowLong(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
+        }
+
+        public void ShowShort(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Short).Show();
+        }
+    }
+}
---
+++ XO/XO/Utilities/IShowToast.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XO.Utilities
+{
+    public interface IShowToast
+    {
+        void ShowLong(string message);
+        void ShowShort(string message);
+    }
+}
